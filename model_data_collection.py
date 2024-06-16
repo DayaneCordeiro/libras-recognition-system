79e930118be153511d1fc3776c812013d83124ca
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2024-06-16 16:55:57 -0300

    feat: rnn tentativa de fazer sem frames

---
+++ model_data_collection.py
@@ -0,0 +1,100 @@
+import cv2
+import numpy as np
+import os
+import mediapipe as mp
+
+# Inicializando as variáveis das libs do mediapipe que vão ajudar no mapeamento da face e da mão
+mp_holistic = mp.solutions.holistic  # Faz as detecções
+mp_drawing = mp.solutions.drawing_utils  # faz os desenhos dos pontos
+
+
+# Função que faz a detecção dos pontos de interesse utilizando as libs do mediapipe
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Converte a imagem de BGR para RGB
+    image.flags.writeable = False  # A imagem ainda não é gravável
+
+    results = model.process(image)  # Faz a predição
+
+    image.flags.writeable = True  # A imagem passa a ser gravável
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Converte a imagem de RGB para BGR
+
+    return image, results
+
+
+# Desenha os pontos-chave da mão direita
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
+# Extração dos pontos-chave
+def extract_key_points(results):
+    return np.array([[res.x, res.y, res.z] for res in
+                     results.right_hand_landmarks.landmark]).flatten() \
+        if results.right_hand_landmarks else np.zeros(21 * 3)
+
+
+# Definição de variáveis que irão coletar os frames para treinar a rede ============================================
+DATA_PATH = os.path.join('dataset')
+
+# Ações e gestos que serão detectados
+classes = np.array(['b'])
+
+# Número de imagens para cada classe
+number_of_images = 400
+
+# cria no sistema operacional uma pasta para cada classe
+for libras_class in classes:
+    for image_number in range(number_of_images):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, libras_class))
+        except:
+            pass
+
+# Acessando a webcam
+cap = cv2.VideoCapture(0)
+
+# Define o modelo do mediapipe
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # Percorre as ações definidas
+    for libras_class in classes:
+        # Coleta os pontos de interesse da mão direita definidos para cada classe
+        for image_number in range(number_of_images):
+            ret, frame = cap.read()
+
+            # Faz a detecção
+            image, results = mediapipe_detection(frame, holistic)
+
+            # Desenhando os pontos de referência
+            draw_landmarks(image, results)
+
+            # Escrita na tela
+            if number_of_images == 0:
+                cv2.putText(image, 'INICIANDO A COLETA DE DADOS', (120, 200),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+                cv2.putText(image, 'Coletando imagens para a classe {} - imagem {}/{}'.format(libras_class,
+                                                                                              image_number,
+                                                                                              number_of_images),
+                            (15, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+            else:
+                cv2.putText(image, 'Coletando frames para a classe {} - video {}/{}'.format(libras_class,
+                                                                                            image_number,
+                                                                                            number_of_images),
+                            (15, 12),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+            cv2.imshow('Reconhecimento de LIBRAS com LSTM Deep Learning', image)
+
+            # Exportando pontos-chave
+            key_points = extract_key_points(results)
+            npy_path = os.path.join(DATA_PATH, libras_class, str(image_number))
+            np.save(npy_path, key_points)
+
+            if cv2.waitKey(10) & 0xff == ord('q'):
+                break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ train_model.py
@@ -0,0 +1,92 @@
+import os.path
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard, Callback
+import numpy as np
+
+DATA_PATH = os.path.join('dataset')
+classes = np.array(['a', 'b'])
+number_of_images = 400
+
+
+class EarlyStoppingByAccuracy(Callback):
+    def __init__(self, monitor='accuracy', value=0.90, verbose=1):
+        super(Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        if current is None:
+            print(f"Monitor '{self.monitor}' is not available. Available metrics are: {', '.join(logs.keys())}")
+            return
+
+        if current >= self.value:
+            if self.verbose > 0:
+                print(
+                    f"Epoch {epoch + 1}: early stopping as {self.monitor} reached {current:.4f} (target was {self.value:.4f})")
+            self.model.stop_training = True
+
+
+# Dicionário com as ações utilizadas no sistema
+dictionary = {label: num for num, label in enumerate(classes)}
+
+# sequences - dados de recursos
+# labels - rótulos
+images, labels = [], []
+
+for libras_class in classes:
+    for image_number in range(number_of_images):
+
+        result = np.load(os.path.join(DATA_PATH, libras_class, "{}.npy".format(image_number)))
+
+        images.append(result)
+        labels.append(dictionary[libras_class])
+
+x = np.array(images)
+y = to_categorical(labels).astype(int)
+
+print(y.shape)
+
+# Dividindo a base entre treino e teste
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+
+# Obtenção de logs para monitorar o treinamento da rede
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+print(x_train.shape)
+print(y_train.shape)
+# Construção arquitetura da rede neural
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(32, 63)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(classes.shape[0], activation='softmax'))
+
+# Compilando o modelo
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+early_stopping_by_accuracy = EarlyStoppingByAccuracy(monitor='accuracy', value=0.90, verbose=1)
+
+
+# Treinando o modelo
+model.fit(
+    x_train,
+    y_train,
+    epochs=2000,
+    callbacks=[tb_callback]
+)
+
+model.summary()
+
+result = model.predict(x_test)
+print("Predito: ", classes[np.argmax(result[0])])
+print("Encontrado pela rede: ", classes[np.argmax(y_test[0])])
+
+model.save('model.h5')
